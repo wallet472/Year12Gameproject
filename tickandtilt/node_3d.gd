47d89d28825004ed87e0ea4f56b41c03abf39350
wallet472 <walabd67@kingshigh.school.nz>
2025-06-10 12:19:11 +1200

    Movement nearly done

--- tickandtilt/node_3d.gd
+++ tickandtilt/node_3d.gd
@@ -3,10 +3,64 @@ extends CharacterBody3D
 @export_group("Camera")
 @export_range(0.0, 1.0) var mouse_sensitivity := 0.25
 
+@export_group("Movement")
+@export var move_speed := 8.0
+@export var acceleratiom := 20.0
+@export var rotation_speed := 12.0
+
 var _camera_input_direction := Vector2.ZERO
+var _last_movement_direction := Vector3.BACK
+
+@onready var _camera_pivot:Node3D = %CameraPivot
+@onready var _camera: Camera3D = %Camera3D
+@onready var _skin: Node3D = %testguy
+
+@onready var anim: AnimationPlayer = $testguy/AnimationPlayer
+
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _unhandled_input(event: InputEvent) -> void:
 	var is_camera_motion := (
 		event is InputEventMouseMotion and
 		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
 	)
+	if  is_camera_motion:
+		_camera_input_direction = event.screen_relative
+		mouse_sensitivity
+
+func _physics_process(delta: float) -> void:
+	_camera_pivot.rotation.x += _camera_input_direction.y * delta
+	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, -PI / 6.0, PI/3.0)
+	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
+
+	_camera_input_direction = Vector2.ZERO
+
+	var raw_input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var forward := _camera.global_basis.z
+	var right := _camera.global_basis.x
+	
+	var move_direction := forward * raw_input.y + right * raw_input.x
+	move_direction.y = 0.0
+	move_direction = move_direction.normalized()
+	
+	velocity = velocity.move_toward(move_direction * move_speed, acceleratiom * delta)
+	move_and_slide()
+	
+	if move_direction.length() >0.2:
+		_last_movement_direction = move_direction
+	var target_angel := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
+	_skin.global_rotation.y = lerp_angle(_skin.rotation.y, target_angel, rotation_speed * delta)
+	
+	var ground_speed := velocity.length()
+	if ground_speed > 0.0:
+		anim.play("local/walk_with_bomb",0.2)
+	else:
+		anim.play("local/idle_with_bomb",0.2)
+	
+	
